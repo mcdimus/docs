19f0ff6adaba501cf8280c16c078a19af41882c1
Dmitri Maksimov <dmitri.maksimov@bytelife.com>
2016-04-13 22:04:17 +0300

    Initial commit

---
+++ src/main/java/ee/skeptic/docs/Main.kt
@@ -0,0 +1,32 @@
+package ee.skeptic.docs
+
+import javafx.application.Application
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+
+class Main : Application() {
+
+    override fun start(primaryStage: Stage) {
+        val btn = Button()
+        btn.text = "Say 'Hello World'"
+        btn.onAction = EventHandler<ActionEvent> { println("Hello World!") }
+
+        val root = StackPane()
+        root.children.add(btn)
+
+        val scene = Scene(root, 300.0, 250.0)
+        primaryStage.title = "Hello World!"
+        primaryStage.scene = scene
+        primaryStage.show()
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            launch(Main::class.java)
+        }
+    }
+}
